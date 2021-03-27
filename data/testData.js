673fa1ec0ca909dff89670d4ff5f60cedcf732e8
TanyaKars <euphratus.tk@gmail.com>
2021-03-27 12:38:50 -0700

    testData1

---
+++ data/testData.js
@@ -0,0 +1,15 @@
+const name = {
+    "default": "SuperGirl(*|*)"
+}
+
+const gender = {
+    "he": 0,
+    "she": 1,
+    "it": 2
+}
+
+const age = {
+    "default": "1234567890"
+}
+
+module.exports = {name, age, gender};
