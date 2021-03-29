8d6fa1f2d9c128d95cc6ff480ee10968b2fd76b8
TanyaKars <euphratus.tk@gmail.com>
2021-03-29 15:32:50 -0700

    isAletr => isDisplayed in negative cases

--- test/extended/name.js
+++ test/extended/name.js
@@ -21,55 +21,56 @@ describe('Positive test cases for the Name field', function () {
 
     it('TC-029 Name field accept one symbol', function () {
         $(sel.name).setValue(name.oneSymbol)
-        let alert = $(sel.error).isDisplayed();
+        let alert = $(sel.errorName).isDisplayed();
         expect(alert).toEqual(false);
     });
 
     it('TC-030 Name field accept 70 symbols', function () {
         $(sel.name).setValue(name.symbols70)
-        let alert = $(sel.error).isDisplayed();
+        let alert = $(sel.errorName).isDisplayed();
         expect(alert).toEqual(false);
     });
 
     it('TC-031 Name field accept letters', function () {
         $(sel.name).setValue(name.allLetters)
-        let alert = $(sel.error).isDisplayed();
+        let alert = $(sel.errorName).isDisplayed();
         expect(alert).toEqual(false);
     });
 
     it('TC-032 Name field accept Upper case', function () {
         $(sel.name).setValue(name.allCapital)
-        let alert = $(sel.error).isDisplayed();
+        let alert = $(sel.errorName).isDisplayed();
         expect(alert).toEqual(false);
     });
 
     it('TC-033 Name field accept Lower case', function () {
         $(sel.name).setValue(name.allLower)
-        let alert = $(sel.error).isDisplayed();
+        let alert = $(sel.errorName).isDisplayed();
         expect(alert).toEqual(false);
+
     });
 
     it('TC-034 Name field accept digits', function () {
         $(sel.name).setValue(name.allNumbers)
-        let alert = $(sel.error).isDisplayed();
+        let alert = $(sel.errorName).isDisplayed();
         expect(alert).toEqual(false);
     });
 
     it('TC-035 Name field accept special symbols', function () {
         $(sel.name).setValue(name.allSymbols)
-        let alert = $(sel.error).isDisplayed();
+        let alert = $(sel.errorName).isDisplayed();
         expect(alert).toEqual(false);
     });
 
     it('TC-036 Name field accept letters with space', function () {
         $(sel.name).setValue(name.lettersWithSpaces)
-        let alert = $(sel.error).isDisplayed();
+        let alert = $(sel.errorName).isDisplayed();
         expect(alert).toEqual(false);
     });
 
     it('TC-037 Name field accept russian letters', function () {
         $(sel.name).setValue(name.russianLetters)
-        let alert = $(sel.error).isDisplayed();
+        let alert = $(sel.errorName).isDisplayed();
         expect(alert).toEqual(false);
     });
 
@@ -82,25 +83,26 @@ describe('Positive test cases for the Name field', function () {
 
     describe('Negative test cases for the name field', function () {
 
-    it('TC-040 Name field do not accept SQL/XML code injections', function () {
-        $(sel.name).setValue(name.code)
-        let alert = $(sel.error).isDisplayed();
-        expect(alert).toEqual(false);
-    });
+    // it('TC-040 Name field do not accept SQL/XML code injections', function () {
+    //     $(sel.name).setValue(name.code)
+    //     let alert = $(sel.errorName).isDisplayed();
+    //     expect(alert).toEqual(false);
+    // });
 
     it('TC-041 Name field do not accept Empty name field', function () {
         $(sel.name).click();
         $(sel.outsideOfAnyField).click();
-        let alert = $(sel.error).isDisplayed();
+        let alert = $(sel.errorName).isDisplayed();
         expect(alert).toEqual(true);
     });
 
     it('TC-042 Name field do not accept 71 symbols', function () {
         $(sel.name).setValue(name.symbols71)
-        browser.isAlertOpen()
+        let alert = $(sel.errorName).isDisplayed();
+        expect(alert).toEqual(true);
     });
 
-    it('TC-044 User left Name input field blank', function () {
+    it('TC-044 User left Name input field blank (Submit button)', function () {
         inputValues4(gender.she, age.default, story.comedy);
         let submitBtn = $(sel.submit).isEnabled();
         expect(submitBtn).toEqual(false);
@@ -108,7 +110,8 @@ describe('Positive test cases for the Name field', function () {
 
     it('TC-045 Name field do not accept spaces only', function () {
         $(sel.name).setValue(name.allSpaces)
-        browser.isAlertOpen()
+        let alert = $(sel.errorName).isDisplayed();
+        expect(alert).toEqual(true);
     });
 
     it('TC-046 Name field do not accept spaces only (Submit button disabled)', function () {
@@ -116,5 +119,4 @@ describe('Positive test cases for the Name field', function () {
         let submitBtn = $(sel.submit).isEnabled();
         expect(submitBtn).toEqual(false);
     });
-
 });
